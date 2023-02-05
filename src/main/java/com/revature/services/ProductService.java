ab8eadd9268b946d2a379f5738684cba9fa2b839
Soni Mohandas <sonireetha@gmail.com>
2023-02-04 20:57:25 -0500

    new changes

--- src/main/java/com/revature/services/ProductService.java
+++ src/main/java/com/revature/services/ProductService.java
@@ -3,6 +3,7 @@ package com.revature.services;
 import com.revature.dtos.ProductInfo;
 import com.revature.models.Product;
 import com.revature.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.Optional;
 public class ProductService {
 
     private final ProductRepository productRepository;
+
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
--- src/test/java/com/revature/services/ProductReviewTest.java
+++ src/test/java/com/revature/services/ProductReviewTest.java
@@ -1,9 +1,6 @@
 package com.revature.services;
 
-
 import com.revature.dtos.ProductReviewRequest;
-import com.revature.dtos.ProductReviewResponse;
-import com.revature.dtos.UserResponse;
 import com.revature.models.Product;
 import com.revature.models.ProductReview;
 import com.revature.models.User;
@@ -20,171 +17,122 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
-import static java.lang.Integer.sum;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ProductReviewTest {
-
     @Mock
     private ProductReviewRepository productReviewRepository;
     @Mock
     private ProductRepository productRepository;
-
     @Mock
     private UserRepository userRepository;
-    @Mock
-    private ProductReview productReview;
-    @Mock
-    private ProductReviewResponse productReviewResponse;
-    @Mock
-    private User user;
-
     @Autowired
     @InjectMocks
-    private ProductReviewService productReviewService;
+    ProductReviewService productReviewService;
+    @Mock
+    ProductService productService;
+    @Mock
+    UserService userService;
+    public List<ProductReview> productReviewMock;
+    public User userMock;
+    public Product productMock;
 
-    @Autowired
-    @InjectMocks
-    private ProductService productService;
-
-    @Autowired
-    @InjectMocks
-    private UserService userService;
-
-    private List<Product> productListMock = Stream.of(
-                new Product(999,1,20,"Valid","img","Tomato",true),
-                new Product(999,1,20,"Valid","img","Potato",true),
-                new Product(999,1,20,"Valid","img","Grapes",true))
-                .collect(Collectors.toList());
-
-    private User userValidMock1 = new User(999,"Valid","Valid","Sam","Ran",true,true,"Valid");
-
-    private List<ProductReview> productReviewMock = Stream.of(
-                new ProductReview(999,5,"Valid", productListMock.get(0), userValidMock1),
-                new ProductReview(999,2,"bad", productListMock.get(0), userValidMock1))
-                .collect(Collectors.toList());
-
-    private List<ProductReviewRequest> productReviewRequestMock= Stream.of(
-                new ProductReviewRequest(999,1,"Valid",999),
-                new ProductReviewRequest(0,0,"",0))
-                .collect(Collectors.toList());
-
-    private ProductReviewResponse  productReviewResponseMock1 = new ProductReviewResponse(999,5,"Valid",999,new UserResponse(userValidMock1));
-    @BeforeEach
-    public void setUp(){
+    @BeforeEach // Every thing inside setup will start before each method
+    void setUp() {
         userService=new UserService(userRepository);
 
         productService= new ProductService(productRepository);
 
         productReviewService= new ProductReviewService(productReviewRepository,userService,productService);
+        // Mock data
+        productReviewMock = new ArrayList<>();
+        productMock = new Product(101, 2, 5, "good", "img", "tomato", true);
+        userMock = new User(201, "email", "pwd", "fname", "lname", false, true, "Authorization");
+        productReviewMock.add(new ProductReview(1,3,"not bad", productMock, userMock));
+        productReviewMock.add(new ProductReview(2,5,"very good", productMock, userMock));
     }
 
-    @AfterEach
-    public void tearDown()
-    {
-        productReviewMock = null;
+    @AfterEach  // After each method run Mock product review will become null
+    void tearDown() {
+        productReviewMock=null;
     }
+
     @DisplayName("Find all product reviews")
     @Test
-    public void findAllProductReviews(){
-        //When
+    void findAll() {
         when(productReviewRepository.findAll()).thenReturn(productReviewMock);
-        //Action
-        List<ProductReviewResponse> productReviews = productReviewService.findAll();
-        verify(productReviewRepository,times(1)).findAll();
-        assertEquals(productReviewMock.size(), productReviews.size());
+        assertEquals(2,productReviewService.findAll().size());
+    }
+    @DisplayName("Find product review by id")
+    @Test
+    void findById() {
+        when(productReviewRepository.findById(1)).thenReturn(Optional.of(productReviewMock.get(0)));
+        assertTrue(productReviewService.findById(1).isPresent());
     }
 
-    @DisplayName("Find product reviews by id")
+    @DisplayName("Find product review by product id")
     @Test
-    public void findProductReviewById(){
-        //Given
-        ArrayList<ProductReview> arr = new ArrayList<>();
-        arr.add(productReviewMock.get(0));
-        arr.add(productReviewMock.get(1));
-        //When
-        lenient().when(productReviewRepository.findById(eq(1))).thenReturn(Optional.ofNullable(arr.get(0)));
-        //Then
-        Optional<ProductReviewResponse> products = productReviewService.findById(999);
-        verify(productReviewRepository,times(1)).findById(999);
+    void findByProductId() {
+        when(productReviewRepository.findAllByProductId(101)).thenReturn(productReviewMock);
+        assertEquals(2, productReviewService.findByProductId(101).size());
+    }
+    @DisplayName("Find product review average score")
+    @Test
+    void findProductAverageScore() {
+        when(productReviewRepository.findProductAverageScore(101)).thenReturn(Arrays.asList(4,6));
+        List<Integer> list = new ArrayList<>(productReviewRepository.findProductAverageScore(101));
+        int avg = list.stream().reduce(0, Integer::sum)/list.size();
+        assertEquals(avg, productReviewService.findProductAverageScore(101));
+    }
+    @DisplayName("Find product review average score - negative testing")
+    @Test
+    void findProductAverageScore_WithListSizeZero() {
+        when(productReviewRepository.findProductAverageScore(102)).thenReturn(Arrays.asList());
+        List<Integer> list = new ArrayList<>(productReviewRepository.findProductAverageScore(102));
+        assertEquals(list.size(), productReviewService.findProductAverageScore(102));
     }
 
-
-    @DisplayName("Find product reviews by product id")
+    @DisplayName("Find product by product id and rating")
     @Test
-    public void testFindAllProductReviewsBySpecificProduct() {
-        //Given
-        ArrayList<ProductReview> arr = new ArrayList<>();
-        arr.add(productReviewMock.get(0));
-        arr.add(productReviewMock.get(1));
-        //When
-        when(productReviewRepository.findAllByProductId(eq(1))).thenReturn(new ArrayList<>());
-        when(productReviewRepository.findAllByProductId(eq(2))).thenReturn(arr);
-        //Then
-        List<ProductReviewResponse> products = productReviewService.findByProductId(1);
-        verify(productReviewRepository,times(1)).findAllByProductId(1);
-        assertEquals(0, products.size());
-
-        products = productReviewService.findByProductId(2);
-        verify(productReviewRepository,times(1)).findAllByProductId(2);
-        assertEquals(2, products.size());
+    void findProductByScore() {
+        when(productReviewRepository.findAllByProductScore(101, 5)).thenReturn(productReviewMock);
+        assertEquals(2, productReviewService.findProductByScore(101, 5).size());
     }
-
-    @DisplayName("Find average product review score")
+    @DisplayName("User id with product id can post")
     @Test
-    public void findProductAverageScore(){
-        //When
-        when(productReviewRepository.findProductAverageScore(999)).thenReturn(Arrays.asList(4,6));
-        //Then
-        int productAvg = productReviewService.findProductAverageScore(999);
-        verify(productReviewRepository, times(1)).findProductAverageScore(999);
-        assertEquals(productAvg, 5);
+    void canPost() {
+        when(productReviewRepository.canPost(101, 201)).thenReturn(productReviewMock);
+        assertFalse(productReviewService.canPost(101, 201));
     }
-    @DisplayName("Find product by score")
+    @DisplayName("Updating product review")
     @Test
-    public void findProductByScore(){
-        //When
-        when(productReviewRepository.findAllByProductScore(999, 5)).thenReturn(new ArrayList<>());
-        //Then
-        List<ProductReviewResponse> list = productReviewService.findProductByScore(999, 5);
-        verify(productReviewRepository,times(1)).findAllByProductScore(999,5);
+    void save() {
 
-    }
-
-    @DisplayName("Can post review or not")
-    @Test
-    public void canPostReviewOrNot(){
-        //When
-        when(productReviewRepository.canPost(999, 999)).thenReturn(productReviewMock);
-        //Then
-        productReviewService.canPost(999, 999);
-        verify(productReviewRepository, times(1)).canPost(999,999);
-    }
-    @DisplayName("Change existing product review")
-    @Test
-    public void save(){
-        when(productReviewRepository.canPost(anyInt(), anyInt())).thenReturn(new ArrayList<>());
+        when(productRepository.findActiveById(101)).thenReturn(Optional.of(new Product()));
         when(productReviewRepository.save(any(ProductReview.class))).thenReturn(new ProductReview());
-        when(productRepository.findActiveById(anyInt())).thenReturn(Optional.of(new Product()));
-        ProductReview p = productReviewService.save(new ProductReviewRequest(1, 4, "Comment", 1), new User());
-        verify(productReviewRepository, times(1)).canPost(anyInt(), anyInt());
+        ProductReview p = productReviewService.save(new ProductReviewRequest(1,4,"good",101), new User());
         verify(productReviewRepository, times(1)).save(any(ProductReview.class));
         assertNotNull(p);
-
     }
-    @DisplayName("Delete By Id")
+    @DisplayName("Return null if rating > 5 - Exception")
     @Test
-    public void deleteById(){
-        //When
-        productReviewService.deleteById(999);
-        //Then
-        verify(productReviewRepository, times(1)).deleteById(eq(999));
+    void doNotSaveRatingMoreThan5() {
+        lenient().when(productReviewRepository.save(any(ProductReview.class)))
+                .thenReturn(new ProductReview()).thenThrow(NullPointerException.class);
+        productReviewService.save(new ProductReviewRequest(1, 7, "", 101), new User());
+    }
+
+    @DisplayName("Delete a product review by id")
+    @Test
+    void deleteById() {
+        productReviewService.deleteById(101);
+        verify(productReviewRepository, times(1)).deleteById(101);
     }
 }
